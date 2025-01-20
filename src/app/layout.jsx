aa58362eb02a7b4e165995818bf661011d03ad88
= <=>
2025-01-19 23:12:42 -0500

    corriegiendo

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <UserProvider>
         <body className={inter.className}>
-      
+
           {children}
           <Footer />
         </body>
